20d27c55a86fdb799568c5c72366e986dbaab7b5
Misha Shaposhnikov <66800817+mishapos@users.noreply.github.com>
2023-03-02 02:32:51 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 cd /bin/staticsites/
-cat /bin/staticsites/swa_metadata.txt
-cat /bin/staticsites/swa_metadata.txt 2>&1
-#./StaticSitesClient $INPUT_ACTION
+./StaticSitesClient $INPUT_ACTION
